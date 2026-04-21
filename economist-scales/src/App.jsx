4ebf4883f074fa5d9d8eec86fbd3d7aace18d097
Constanze Bayer <39796792+looreen@users.noreply.github.com>
2026-04-21 18:55:24 +0200

    current status economist chart

--- economist-scales/src/App.jsx
+++ economist-scales/src/App.jsx
@@ -1,121 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { scaleBand, scaleLinear } from "d3";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const data = [
+    { count: 6, name: "Hantavirus" },
+    { count: 7, name: "Tularemia" },
+    { count: 7, name: "Dengue" },
+    { count: 9, name: "Ebola" },
+    { count: 11, name: "E. coli" },
+    { count: 15, name: "Tuberculosis" },
+    { count: 17, name: "Salmonella" },
+    { count: 18, name: "Vaccinia" },
+    { count: 54, name: "Brucella" },
+  ];
+
+  const width = 650;
+  const height = 360;
+
+  const xScale = scaleLinear().range([0, width]).domain([0, 55]);
+  const yScale = scaleBand()
+    .range([height, 0])
+    .domain([
+      "Hantavirus",
+      "Tularemia",
+      "Dengue",
+      "Ebola",
+      "E. coli",
+      "Tuberculosis",
+      "Salmonella",
+      "Vaccinia",
+      "Brucella",
+    ])
+    .padding(0.5);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <div style={{ width: width, padding: "0px 20px" }}>
+        <div style={{ height: "100px", width: width }}>
+          <div
+            style={{
+              marginTop: "14px",
+              width: "100%",
+              height: "1px",
+              backgroundColor: "#e5011c",
+            }}
+          ></div>
+          <div
+            style={{ width: "36px", height: "9px", backgroundColor: "#e5011c" }}
+          ></div>
+          <span
+            style={{ display: "block", fontSize: "20px", textAlign: "left" }}
+          >
+            <b>Escape Artists</b>
+          </span>
+          <br></br>
+          <span
+            style={{ display: "block", fontSize: "16px", textAlign: "left" }}
+          >
+            Number of laboratory-acquired infections, 1970-2021
+          </span>
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
+          <svg width={width} height={height}>
+            <rect width={width} height={height} fill={"lightgrey"} />
+            {data.map((d, i) => (
+              <g>
+                <rect
+                  key={i}
+                  x={0}
+                  y={yScale(d.name)}
+                  height={yScale.bandwidth()}
+                  width={xScale(d.count)}
+                  fill={"#076fa2"}
+                  stroke="#076fa2"
+                />
+                <text
+                  fill={d.count > 7 ? "white" : "#076fa2"}
+                  fill-opacity={0.9}
+                  y={yScale(d.name)}
+                  x={d.count > 7 ? xScale(7) : xScale(d.count + 4)}
+                  textAnchor="start"
+                  alignmentBaseline="central"
+                  fontSize={14}
+                >
+                  {d.name}
+                </text>
+              </g>
+            ))}
+            <rect></rect>
           </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
     </>
-  )
+  );
 }
-
-export default App
